8de495534be8a59b4f5f2f455de65180784aecfe
unknown <eaglesa@EAS-BEN016C4.it.muohio.edu>
2014-11-11 15:30:55 -0500

    Moved Heap.ipp (+ constructor, grow, numitems)

--- Heap.h
+++ Heap.h
@@ -46,4 +46,55 @@ private:
   void trickleDown(unsigned long index);  
 };
 
-#include "Heap.ipp"
+#include <string>
+
+template<class Pri, class T>
+Heap<Pri,T>::Heap(){
+  arrSize=20;
+  numItems=0;
+  backingArray = new std::pair<Pri, T>[arrSize]; 
+}
+
+template<class Pri, class T>
+Heap<Pri,T>::~Heap(){
+  //destructor
+}
+
+template<class Pri, class T>
+void Heap<Pri,T>::grow(){
+  std::pair<Pri, T>* tempArray = new std::pair<Pri, T>[arrSize*2];
+  for(int i=0; i<arrSize+1; i++)
+  { tempArray[i] = backingArray[i]; }
+  backingArray = tempArray;
+  delete[] tempArray; 
+}
+
+template<class Pri, class T>
+void Heap<Pri,T>::add(std::pair<Pri,T> toAdd){
+  if(numItems>=arrSize)
+  { grow(); }
+  // add into array and bubble up at some point
+}
+
+template<class Pri, class T>
+void Heap<Pri,T>::bubbleUp(unsigned long index){
+  //TODO
+}
+
+template<class Pri, class T>
+void Heap<Pri,T>::trickleDown(unsigned long index){
+  //TODO
+}
+
+template<class Pri, class T>
+std::pair<Pri,T> Heap<Pri,T>::remove(){
+  //TODO
+  std::pair<Pri,T> tmp;
+  return tmp;
+}
+
+template<class Pri, class T>
+unsigned long Heap<Pri,T>::getNumItems(){
+  return numItems;
+}
+
